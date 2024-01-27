3862d01ede732fa48a07a9a9593930f6a69f102d
Ruslan Konysheu <timintal@gmail.com>
2024-01-27 14:02:32 +0100

    updates

--- src/main.rs
+++ src/main.rs
@@ -9,6 +9,8 @@ enum Route {
     Home,
     #[at("/privacy/*path")]
     Privacy { path: String },
+    #[at("/*path")]
+    Other {path: String},
     #[not_found]
     #[at("/404")]
     NotFound,
@@ -18,6 +20,7 @@ fn switch(routes: Route) -> Html {
     match routes {
         Route::Home => html! { <h1>{ "Home Page" }</h1> },
         Route::Privacy { path } => privacy_page::get_privacy_page(path.replace("_", " ").as_str()),
+        Route::Other { path } => html! { <h1>{ format!("Other Page {path}", path=path) }</h1> },
 
         Route::NotFound => html! { <h1>{ "404. Page Not Found" }</h1> },
     }
--- src/privacy_page.rs
+++ src/privacy_page.rs
@@ -8,29 +8,40 @@ pub(crate) fn get_privacy_page(name:&str) -> Html {
             <h3>{"Effective Date: 26th January, 2024"}</h3>
         <p>{format!("This Privacy Policy describes how your personal information is collected, used, and shared when you install or use the {name} game (the \"Game\") on any mobile device. This Game is developed by Ruslan Konysheu, an individual developer residing in Spain (\"we\", \"us\", \"our\").By installing and using the Game, you agree to the collection and use of your information in accordance with this Privacy Policy. If you do not agree with the terms of this policy, please do not install or use the Game.")}</p>
         <h2>{"1. Information We Collect"}</h2>
-        <p>{"When you install and use the Game, we may collect certain information automatically, including but not limited to:
+        <p>{"When you install and use the Game, we may collect certain information automatically, including but not limited to:"}</p>
 
-Device Information: We collect information about the device you use to access the Game, including the hardware model, operating system and version, unique device identifiers, and mobile network information.
+<p>{"Device Information: We collect information about the device you use to access the Game, including the hardware model, operating system and version, unique device identifiers, and mobile network information."}</p>
 
-Location Information: We may use your IP address to estimate your general location.
+<p>{"Location Information: We may use your IP address to estimate your general location."}</p>
 
-Usage Information: We collect information about your interaction with the Game, such as app launches, taps, video views, and other usage statistics.
+<p>{"Usage Information: We collect information about your interaction with the Game, such as app launches, taps, video views, and other usage statistics."}</p>
 
-Diagnostic Information: We may collect information related to the performance of the Game and the Admob SDK, including crash logs, app launch time, hang rate, and energy usage.
+<p>{"Diagnostic Information: We may collect information related to the performance of the Game and the Admob SDK, including crash logs, app launch time, hang rate, and energy usage."}</p>
 
-Identifiers: We collect device and account identifiers to provide our services, including advertising and in-app purchases.
+<p>{"Identifiers: We collect device and account identifiers to provide our services, including advertising and in-app purchases.
 "}</p>
         
         <h2>{"2. How We Use Your Information"}</h2>
-        <p>{"We use the information we collect to:
-
-Operate and maintain the Game;
+        <p>{"We use the information we collect to:"}</p>
+        <p>{format!("Operate and maintain the Game;
 Provide customer support and respond to your queries;
 Send you technical notices, updates, security alerts, and support and administrative messages;
 Monitor and analyze trends, usage, and activities in connection with the Game;
-Detect, investigate and prevent fraudulent transactions and other illegal activities and protect the rights and property of Drop Craft and others;
+Detect, investigate and prevent fraudulent transactions and other illegal activities and protect the rights and property of {name} and others;
 Personalize and improve the Game and provide advertisements, content, or features that match user profiles or interests.
-"}</p>
+")}</p>
+        <h2>{"3. Sharing of Information"}</h2>
+        
+        <p>{"We may share your information as follows or as otherwise described in this Privacy Policy:" }</p>
+        <p>{format!("
+
+With vendors, consultants, and other service providers who need access to such information to carry out work on our behalf;
+In response to a request for information if we believe disclosure is in accordance with, or required by, any applicable law, regulation, or legal process;
+If we believe your actions are inconsistent with our user agreements or policies, or to protect the rights, property, and safety of {name} or others;
+In connection with, or during negotiations of, any merger, sale of company assets, financing, or acquisition of all or a portion of our business by another company;
+Between and among Drop Craft and our current and future parents, affiliates, subsidiaries, and other companies under common control and ownership; and
+With your consent or at your direction.
+")}</p>
         </>
     }
 }

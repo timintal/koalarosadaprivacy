dc494f716edf473392c32def17d25523d8ca2f72
Ruslan Konysheu <timintal@gmail.com>
2024-01-27 10:01:21 +0100

    added home page

--- src/main.rs
+++ src/main.rs
@@ -5,8 +5,10 @@ use yew::prelude::*;
 
 #[derive(Clone, Routable, PartialEq)]
 enum Route {
+    #[at("/")]
+    Home,
     #[at("/privacy/*path")]
-    Home { path: String },
+    Privacy { path: String },
     #[not_found]
     #[at("/404")]
     NotFound,
@@ -14,7 +16,8 @@ enum Route {
 
 fn switch(routes: Route) -> Html {
     match routes {
-        Route::Home { path } => privacy_page::get_privacy_page(path.replace("_", " ").as_str()),
+        Route::Home => html! { <h1>{ "Home Page" }</h1> },
+        Route::Privacy { path } => privacy_page::get_privacy_page(path.replace("_", " ").as_str()),
 
         Route::NotFound => html! { <h1>{ "404. Page Not Found" }</h1> },
     }

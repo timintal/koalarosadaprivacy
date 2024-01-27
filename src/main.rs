35e13ebc0cc89e375f7216ac809b7fd99a65c79b
Ruslan Konysheu <timintal@gmail.com>
2024-01-27 14:16:22 +0100

    privacy path  added

--- src/main.rs
+++ src/main.rs
@@ -7,10 +7,8 @@ use yew::prelude::*;
 enum Route {
     #[at("/koalarosadaprivacy")]
     Home,
-    #[at("/privacy/*path")]
+    #[at("/koalarosadaprivacy/*path")]
     Privacy { path: String },
-    #[at("/*path")]
-    Other {path: String},
     #[not_found]
     #[at("/404")]
     NotFound,
@@ -20,7 +18,6 @@ fn switch(routes: Route) -> Html {
     match routes {
         Route::Home => html! { <h1>{ "Home Page" }</h1> },
         Route::Privacy { path } => privacy_page::get_privacy_page(path.replace("_", " ").as_str()),
-        Route::Other { path } => html! { <h1>{ format!("Other Page {path}", path=path) }</h1> },
 
         Route::NotFound => html! { <h1>{ "404. Page Not Found" }</h1> },
     }
